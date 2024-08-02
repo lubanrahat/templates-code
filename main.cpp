253669413c9e9a890e189f4a51d990a06b326c52
Luban Rahat <153543408+lubanrahat@users.noreply.github.com>
2024-08-02 10:21:23 +0600

    cp templates code

---
+++ main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <limits>
+#include <cmath>
+#include <algorithm>
+#include <vector>
+#include <list>
+#include <stack>
+#include <queue>
+#include <array>
+#include <set>
+#include <map>
+#include <utility>
+using namespace std;
+typedef long long i64;
+typedef unsigned long ul;
+#define PI 3.1415926535897932384626
+#define rep(i,n) for(int i = 0;i < n;i++)
+#define tr(it, a) for (auto it = a.begin(); it != a.end(); it++)
+
+int main() {
+    
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    
+    
+    return 0;
+}
